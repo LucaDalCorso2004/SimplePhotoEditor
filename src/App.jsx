dcaf27e07e8307392df6ac101291dcb155c64f74
notmhx <michaelherrmann111104@gmail.com>
2023-09-05 13:17:58 +0200

    working refactoring with chatgpt

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,55 @@ import { useState } from "react";
 import "./App.css";
 import { Slider } from "./Slider.jsx";
 
+const defaultFilters = {
+  brightness: 100,
+  sepia: 0,
+  blur: 0,
+  contrast: 100,
+  saturate: 100,
+  hueRotate: 0,
+  invert: 0,
+  opacity: 100,
+  grayscale: 0,
+};
+
+const filterFunctions = {
+  brightness: "brightness",
+  sepia: "sepia",
+  blur: "blur",
+  contrast: "contrast",
+  saturate: "saturate",
+  hueRotate: "hue-rotate",
+  invert: "invert",
+  opacity: "opacity",
+  grayscale: "grayscale",
+};
+
 function App() {
   const [currentURL, setURL] = useState("./src/assets/default.jpg");
-  const [currentBrightness, setBrightness] = useState(100);
-  const [currentSepia, setSepia] = useState(0);
-  const [currentBlur, setBlur] = useState(0);
-  const [currentContrast, setContrast] = useState(100);
-  const [currentSaturate, setSaturate] = useState(100);
-  const [currentHueRotate, setHueRotate] = useState(0);
-  const [currentInvert, setInvert] = useState(0);
-  const [currentOpacity, setOpacity] = useState(100);
-  const [currentGrayscale, setGrayscale] = useState(0);
+  const [filters, setFilters] = useState(defaultFilters);
 
   function addImage(url) {
-    if (url == "") return;
+    if (url === "") return;
     setURL(url);
   }
 
   const filterStyle = {
-    filter: `brightness(${currentBrightness}%) sepia(${currentSepia}%) blur(${currentBlur}px) contrast(${currentContrast}%) saturate(${currentSaturate}%) hue-rotate(${currentHueRotate}deg) invert(${currentInvert}%) opacity(${currentOpacity}%) grayscale(${currentGrayscale}%)`,
+    filter: Object.entries(filters)
+      .map(
+        ([filter, value]) =>
+          `${filterFunctions[filter]}(${value}${
+            filter === "blur" ? "px" : filter === "hueRotate" ? "deg" : "%"
+          })`
+      )
+      .join(" "),
+  };
+
+  const handleFilterChange = (filter, value) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [filter]: value,
+    }));
   };
 
   return (
@@ -33,99 +63,31 @@ function App() {
           placeholder="Input URL"
         ></input>
         <h3>Preview</h3>
-        <img className="image" src={currentURL} style={filterStyle}></img>
+        <img
+          className="image"
+          src={currentURL}
+          style={filterStyle}
+          alt="Filtered"
+        />
       </div>
 
       <div className="box">
-        <Slider
-          handleValueChange={(value) => {
-            setBrightness(value);
-          }}
-          defaultValue={100}
-          maxValue={200}
-          minValue={0}
-        />
-        <p>Brightness: {currentBrightness}%</p>
-
-        <Slider
-          handleValueChange={(value) => {
-            setSepia(value);
-          }}
-          defaultValue={0}
-          maxValue={100}
-          minValue={0}
-        />
-        <p>Sepia: {currentSepia}%</p>
-
-        <Slider
-          handleValueChange={(value) => {
-            setBlur(value);
-          }}
-          defaultValue={0}
-          maxValue={200}
-          minValue={0}
-        />
-        <p>Blur: {currentBlur}px</p>
-
-        <Slider
-          handleValueChange={(value) => {
-            setContrast(value);
-          }}
-          defaultValue={100}
-          maxValue={200}
-          minValue={0}
-        />
-        <p>Contrast: {currentContrast}%</p>
-
-        <Slider
-          handleValueChange={(value) => {
-            setSaturate(value);
-          }}
-          defaultValue={100}
-          maxValue={200}
-          minValue={0}
-        />
-        <p>Saturate: {currentSaturate}%</p>
-
-        <Slider
-          handleValueChange={(value) => {
-            setHueRotate(value);
-          }}
-          defaultValue={0}
-          maxValue={360}
-          minValue={0}
-        />
-        <p>Hue-Rotate: {currentHueRotate}deg</p>
-
-        <Slider
-          handleValueChange={(value) => {
-            setInvert(value);
-          }}
-          defaultValue={0}
-          maxValue={100}
-          minValue={0}
-        />
-        <p>Invert: {currentInvert}%</p>
-
-        <Slider
-          handleValueChange={(value) => {
-            setOpacity(value);
-          }}
-          defaultValue={100}
-          maxValue={100}
-          minValue={0}
-        />
-        <p>Opacity: {currentOpacity}%</p>
-        
-        <Slider
-          handleValueChange={(value) => {
-            setGrayscale(value);
-          }}
-          defaultValue={0}
-          maxValue={100}
-          minValue={0}
-        />
-        <p>Grayscale: {currentGrayscale}%</p>
+        {Object.entries(filters).map(([filter, value]) => (
+          <div key={filter}>
+            <Slider
+              handleValueChange={(newValue) => {
+                handleFilterChange(filter, newValue);
+              }}
+              defaultValue={value}
+              maxValue={filter === "blur" || filter === "hueRotate" ? 200 : 100}
+              minValue={0}
+            />
+            <p>
+              {filter.charAt(0).toUpperCase() + filter.slice(1)}: {value}
+              {filter === "blur" ? "px" : filter === "hueRotate" ? "deg" : "%"}
+            </p>
+          </div>
+        ))}
       </div>
     </>
   );
